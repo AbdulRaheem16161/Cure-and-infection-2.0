7cad870722812cabd7980ed87c45f408d9911f06
AbdulRaheem <abdulraheem16161@gmail.com>
2026-02-14 10:29:43 +0500

    organized project files

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Cure-and-infection-2.0
 - Unity version: Unity 6000.0.48f1
-- Active game object:
-  - Name: Shooter(Clone)
-  - Tag: Team 3
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/_Project/_Dev/Temp/experiment.cs
@@ -0,0 +1,25 @@
+//using NUnit.Framework;
+//using UnityEngine;
+//using System.Collections.Generic;
+
+//public class experiment : MonoBehaviour
+//{
+//    public List<GameObject> MarchingFOllowPoints = new List<GameObject>();
+//    public GameObject ClossestPoint;
+
+//    // Update is called once per frame
+//    void Awake()
+//    {
+//        int index = 0;
+//        float clossestDistance = 0f;
+
+//        while (true)
+//        {
+//            if (clossestDistance < Vector3.Distance(transform.position, MarchingFOllowPoints[i].transform.position))
+//            {
+//                ClossestPoint = MarchingFOllowPoints[i];
+//                index++;
+//            }
+//        }
+//    }
+//}
